58bf2572711653445fb0ef434601cadfd18c536f
Ivan Volkov <angrycore@gmail.com>
2023-05-08 11:00:45 +0300

    += Pavlov alias, -= broken docx printer

--- tfs_excel.py
+++ tfs_excel.py
@@ -18,8 +18,8 @@ def main():
     with ExcelPrinter(a.out, vars(a)["from"], vars(a)["to"]) as p:
         p.print(Matrix(i, a.names_reference), a.predefined_spend)
 
-    test = DocxPrinter()
-    test.create_zip(Matrix(i, a.names_reference))
+    # test = DocxPrinter()
+    # test.create_zip(Matrix(i, a.names_reference))
 
     if (a.open):
         if platform in ("linux", "linux2"):
