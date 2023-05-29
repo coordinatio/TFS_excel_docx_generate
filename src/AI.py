0a2ada537542fba36fbe50af185097e2341fa868
Ivan Volkov <angrycore@gmail.com>
2023-05-29 19:39:45 +0300

    Checkpoint.

--- src/AI.py
+++ src/AI.py
@@ -1,4 +1,9 @@
 from typing import List, Tuple
+from pathlib import Path
+from sqlite3 import connect
+
+from tfs import deepcopy
+
 from src.Task import Task
 
 
@@ -14,6 +19,41 @@ class FastStorage:
 
     def memorize_essense(self, tasks: List[Task]):
         raise NotImplementedError
+
+class SQlite(FastStorage):
+    def __init__(self, path_db: str) -> None:
+        self.db = Path(path_db)
+        if not self.db.exists() or self.db.stat().st_size == 0:
+            if not self.db.parent.exists() or not self.db.parent.is_dir():
+                raise ValueError("Invalid SQlite DB path")
+            con = connect(self.db)
+            con.execute(("CREATE TABLE essence_cache ("
+                         "   project TEXT, "
+                         "   tid TEXT, "
+                         "   parent_title TEXT, "
+                         "   title TEXT, "
+                         "   essence TEXT, "
+                         "   PRIMARY KEY (project, tid)"
+                         ");"))
+            con.close()
+        self.con = connect(self.db)
+
+    def read_essense(self, tasks: List[Task]) -> Tuple[List[Task], List[Task]]:
+        known: List[Task] = []
+        unknown: List[Task] = []
+        for t in tasks:
+            q = "SELECT essence FROM essence_cache WHERE project=? AND tid=?"
+            e = self.con.execute(q, (t.project, t.tid)).fetchone()
+            c = deepcopy(t)
+            if not e:
+                unknown.append(c)
+            else:
+                c.essence = e[0]
+                known.append(c)
+        return (known, unknown)
+
+
+
 
 class AI:
     def generate_essense(self, tasks: List[Task]) -> List[Task]:
--- src/test_AI.py
+++ src/test_AI.py
@@ -1,8 +1,9 @@
 from typing import List, Tuple
 from unittest import TestCase
 from copy import deepcopy
+from tempfile import mkstemp
 
-from src.AI import FastStorage, AI, Cache
+from src.AI import FastStorage, SQlite, AI, Cache
 from src.Task import Task
 
 
@@ -45,7 +46,7 @@ class TestCache(TestCase):
 
         ids_out = {t.tid for t in t_out}
         # the length equals and all ids are unique
-        self.assertEqual(ids_out, ids_in) # if True - thus nothing is lost
+        self.assertEqual(ids_out, ids_in)  # if True - thus nothing is lost
 
         z_in = []
         for t in t_out:
@@ -61,3 +62,27 @@ class TestCache(TestCase):
         self.assertEqual({t.tid for t in z_in}, {t.tid for t in z_out})
         for z in z_out:
             self.assertEqual(z.essence, f'KNOWN_{z.tid}_KNOWN')
+
+
+class TestFastStorage(TestCase):
+    def test_happyday(self):
+        a = {'assignees': [], 'release': '', 'link': ''}
+        t = []
+        for i in range(1, 4):
+            t.append(Task(
+                **a, tid=f'{i}{i}{i}{i}', title=f'X{i}X{i}', parent_title=f'X{i}X0', project='X'))
+            t.append(Task(
+                **a, tid=f'{i}{i}{i}{i}', title=f'Y{i}Y{i}', parent_title=f'Y{i}Y0', project='Y'))
+        s = SQlite(mkstemp(suffix='.db')[1])
+
+        known, unknown = s.read_essense(deepcopy(t))
+
+        self.assertEqual(len(t), len(unknown))
+        self.assertEqual(0, len(known))
+
+        for unk in unknown:
+            unk.essence = "Съешь ещё этих мягких французских булок да выпей же чаю."
+        s.memorize_essense(unknown)
+
+        t.append(Task(**a, tid='7777', title='77', parent_title='70', project='Y'))
+
